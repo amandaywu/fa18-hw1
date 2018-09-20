060d2b190f7f9d2315a0082de5181014440841f6
Amanda Wu <amanda.wu@berkeley.edu>
2018-09-19 22:46:46 -0700

    add hw1

--- foobar.rb
+++ foobar.rb
@@ -4,5 +4,12 @@ class Foobar
     # Class method, not an instance method
     # Call with `Foobar.baz`
     # Q4 CODE HERE
+    a.map! {|i| i.to_i + 2}
+    a.select! {|i| i % 2 == 0}
+    a.uniq!
+    a.delete_if {|i| i >= 10}
+    ret = 0
+    a.each {|i| ret += i} 
+    ret
   end
 end
